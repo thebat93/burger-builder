d8e5e9029cffaca91c5a27ba3f15f926a59b3e43
thebat93 <thebat93@sibmail.com>
2018-01-02 00:55:08 +0300

    Outputting Burger Ingredients Dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,11 +6,21 @@ import classes from './Burger.css';
 // компонент бургера
 
 const burger = (props) => {
+    // динамически формируем JSX из компонентов <BurgerIngredient>, который будет находится между нижней и верхей булочкой
+    // преобразуем переданный в props объект в массив
+    const transformedIngredients = Object.keys(props.ingredients)
+        // для каждого типа ингредиента создаем соответствующий компонент <BurgerIngredient> в JSX
+        .map(ingredientKey => {
+            // создаем массив с количеством ячеек, равным количеству ингредиентов определенного типа
+            return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+                // проходим по каждому из низ и возвращаем JSX
+                return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />;
+            });
+        });
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
