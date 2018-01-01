430c255d70de2d672c13e2be7a8872706da1cab0
thebat93 <thebat93@sibmail.com>
2018-01-02 01:26:01 +0300

    Adding the Build Control Component

---
+++ src/components/Burger/BurgerControls/BurgerControls.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// контролы пользователя для выбора ингредиентов бургера
+
+const buildControls = (props) => (
+    <div>
+        
+    </div>
+);
+
+export default buildControls;
