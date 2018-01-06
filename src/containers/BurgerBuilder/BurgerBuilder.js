0643cd6f5c609d55e0bfd5f3a3f1f6663a188aef
thebat93 <thebat93@sibmail.com>
2018-01-06 14:11:03 +0300

    Connecting State to Build Controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,14 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 // контейнер для строителя бургера
 
+// константа - цены ингредиентов
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    bacon: 0.7,
+    cheese: 0.4,
+    meat: 1.3
+}
+
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
@@ -13,13 +21,46 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4 // цена по дефолту
     }
+
+    // обработчик добавления ингредиента
+    // передается через props к BuildControl
+    addIngredientHandler = (type) => {
+        // старое число ингредиентов
+        const oldCount = this.state.ingredients[type];
+        // обновленнное число ингредиентов
+        const updatedCount = oldCount + 1;
+        // копируем объект с ингредиентами
+        const updateIngredients = {
+            ...this.state.ingredients
+        };
+        // обновляем число ингредиентов нужного типа
+        updateIngredients[type] = updatedCount;
+        // добавка к цене для 1 штуки ингрелиента нужного типа
+        const priceAddition = INGREDIENT_PRICES[type];
+        // старая цена
+        const oldPrice = this.state.totalPrice;
+        // новая цена
+        const newPrice = oldPrice + priceAddition;
+        // обновляем состояние
+        this.setState({
+            ingredients: updateIngredients,
+            totalPrice: newPrice
+        });
+    }
+
+    // обработчик удаления ингридиента
+    removeIngredientHandler = (type) => {
+
+    }
+
     render() {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
-                <BuildControls />
+                <BuildControls ingredientAdded={this.addIngredientHandler} />
             </Aux>
         );
     }
