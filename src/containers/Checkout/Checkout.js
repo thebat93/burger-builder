ef3c34a75c881d932a7c1758577c53e616ecd722
thebat93 <thebat93@sibmail.com>
2018-01-16 14:52:45 +0300

    Navigating to the ContactData Components

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 // Детали заказа (контейнер)
 
@@ -40,6 +42,7 @@ class Checkout extends Component {
                     ingredients={this.state.ingredients}
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler} />
+                <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
             </div>
         );
     }
