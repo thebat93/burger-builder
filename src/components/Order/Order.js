cba40b58e5098718067ecc25b7d84344053d26f2
thebat93 <thebat93@sibmail.com>
2018-01-17 15:43:30 +0300

    Outputting the Orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -5,10 +5,31 @@ import classes from './Order.css';
 // заказ
 
 const order = (props) => {
+    const ingredients = [];
+    for (let ingredientName in props.ingredients) { // формируем массив из объектов
+        ingredients.push({
+            name: ingredientName,
+            amount: props.ingredients[ingredientName]
+        });
+    }
+
+    const ingredientOutput = ingredients.map(ingredient => // формируем JSX из ингредиентов
+        <span 
+            style={{
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #ccc',
+                padding: '5px' 
+            }}
+            key={ingredient.name}>
+            {ingredient.name} ({ingredient.amount})
+        </span>
+    );
     return (
         <div className={classes.Order}>
-            <p>Ingredients: Salad (1)</p>
-            <p>Price: <strong>$5</strong></p>
+            <p>Ingredients: { ingredientOutput }</p>
+            <p>Price: <strong>${props.price.toFixed(2)}</strong></p>
         </div>
     );
 }
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -32,8 +32,12 @@ class Orders extends Component {
     render () {
         return (
             <div>
-                <Order />
-                <Order />
+                { this.state.orders.map( order => 
+                    <Order 
+                        ingredients={order.ingredients}
+                        key={order.id}
+                        price={+order.price} />
+                )}
             </div>       
         );
     }
