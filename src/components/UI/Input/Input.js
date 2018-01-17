a026ba423cebe3be873b3b5bb623d6d9887cd550
thebat93 <thebat93@sibmail.com>
2018-01-17 17:41:25 +0300

    Handling User Input

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -8,16 +8,26 @@ const input = (props) => {
     let inputElement = null;
     switch ( props.elementType ) { // разные элементы в зависимости от типа
         case ('input'):
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} />;
+            inputElement = (<input 
+                className={classes.InputElement}
+                {...props.elementConfig} 
+                value={props.value} 
+                onChange={props.changed} />
+            );
             break;
         case ('textarea'):
-            inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value} />;
+            inputElement = (<textarea 
+                className={classes.InputElement} 
+                {...props.elementConfig} 
+                value={props.value} 
+                onChange={props.changed} />
+            );
             break;
         case ('select'):
             inputElement = (
                 <select 
                     className={classes.InputElement}
-                    value={props.value}>
+                    value={props.value} onChange={props.changed} >
                     {props.elementConfig.options.map(option => (
                         <option key={option.value} value={option.value}>{option.displayValue}</option>
                     ))}
@@ -25,7 +35,11 @@ const input = (props) => {
             );
             break;
         default:
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} />;
+            inputElement = (<input 
+                className={classes.InputElement} 
+                {...props.elementConfig} 
+                value={props.value} 
+                onChange={props.changed} />);
     }
 
     return (
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -63,6 +63,18 @@ class ContactData extends Component {
         loading: false
     }
 
+    inputChangedHandler = (e, inputIdentifier) => { // обработчик изменения значения инпутов
+        const updatedOrderForm = { // копируем настройки всей формы
+            ...this.state.orderForm
+        };
+        const updatedFormElement = { // копируем настройки конкретного элемента, который мы меняем
+            ...updatedOrderForm[inputIdentifier]
+        };
+        updatedFormElement.value = e.target.value; // присваиваем новое значение
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        this.setState({ orderForm: updatedOrderForm });
+    }
+
     orderHandler = (e) => { // обработчик нажатия на кнопку "заказать"
         e.preventDefault();
         this.setState({ loading: true }); // меняем состояние загрузки
@@ -97,7 +109,8 @@ class ContactData extends Component {
                         key={formElement.id}
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
-                        value={formElement.config.value} />
+                        value={formElement.config.value}
+                        changed={(e) => this.inputChangedHandler(e, formElement.id)} />
                 ))}
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>  
             </form>
