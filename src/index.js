c305ee9d2787175fcde599841f4507eac1426645
thebat93 <thebat93@sibmail.com>
2018-01-19 17:19:44 +0300

    Basic Redux Setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// компонент для Роутера
 import { BrowserRouter } from 'react-router-dom';
+// компонент для Редакса
+import { Provider } from 'react-redux';
+// функция createStore
+import { createStore } from 'redux';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './store/reducer';
 
-// обернули все приложение в браузер роутер
+// создаем стор с помощью createStore и передаем редьюсер
+const store = createStore(reducer);
+
+// обернули все приложение в браузер роутер и провайдер
+// стор передается в провайдер с помощью пропса store
 
 const app = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
