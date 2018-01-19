cd66ca1143e84b328961d62be4472c71d46d49fc
thebat93 <thebat93@sibmail.com>
2018-01-19 17:56:38 +0300

    Updating Arrays Immutably

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,13 +31,14 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount () {
-        axios.get('https://react-burger-builder-98f3a.firebaseio.com/ingredients.json')
-            .then(response => {
-                this.setState({ ingredients: response.data })
-            })
-            .catch(error => {
-                this.setState({error: true});
-            }); // просто ловим ошибку чтобы отработал HOC обработчик ошибок
+        console.log(this.props);
+        // axios.get('https://react-burger-builder-98f3a.firebaseio.com/ingredients.json')
+        //     .then(response => {
+        //         this.setState({ ingredients: response.data })
+        //     })
+        //     .catch(error => {
+        //         this.setState({error: true});
+        //     }); // просто ловим ошибку чтобы отработал HOC обработчик ошибок
     }
 
     // проверка возможности покупки
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,14 +3,46 @@ import * as actionTypes from './actions';
 
 // изначальное состояние
 const initialState = {
-    ingredients: null,
+    ingredients: {
+        salad: 0,
+        cheese: 0,
+        bacon: 0,
+        meat: 0
+    },
     totalPrice: 4
 }
 
 // редьюсер
 // передаем в функцию состояние (по дефолту изначальное) и экшен
 const reducer = (state = initialState, action) => {
-
+    // ветвление в зависимости от типа экшена
+    // всегда возращаем state
+    switch (action.type) {
+        case actionTypes.ADD_INGRIDIENTS:
+            // меняем state иммутабельно: возвращаем новый объект
+            return {
+                ...state, // копируем все свойства на первом уровне 
+                ingredients: {
+                    ...state.ingredients, // копируем все свойства на уровне ingredients
+                    // обновляем свойство выбранного ингредиента
+                    // название свойства передается в экшене
+                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+                }
+            }
+        case actionTypes.REMOVE_INGRIDIENTS:
+        return {
+            ...state, // копируем все свойства на первом уровне 
+            ingredients: {
+                ...state.ingredients, // копируем все свойства на уровне ingredients
+                // обновляем свойство выбранного ингредиента
+                // название свойства передается в экшене
+                [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+            }
+        }
+        // если ни один тип не подошел, то возвращаем старый state
+        default:
+            return state;
+    }
 }
 
 export default reducer;
