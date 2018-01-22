b86cc1e434ae6d938858bec28c8147f7dd69ec1b
thebat93 <thebat93@sibmail.com>
2018-01-22 11:34:12 +0300

    Executing Async Code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// компонент для Роутера
+// компонент для Роутера из React Router
 import { BrowserRouter } from 'react-router-dom';
-// компонент для Редакса
+// компонент для Редакса 
 import { Provider } from 'react-redux';
-// функция createStore
-import { createStore } from 'redux';
+// функции createStore (создание Store) и applyMiddleware (использование Middleware в качестве enhancer)
+// compose (для объединения нескольких Middleware) из Redux
+import { createStore, applyMiddleware, compose } from 'redux';
+// Middleware для добавления асинхронности в Redux
+import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 // создаем стор с помощью createStore и передаем редьюсер
-// второй аргумент передается для работы Redux DevTools
-const store = createStore(burgerBuilderReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+// второй аргумент передается для работы Redux DevTools и thunk
+const store = createStore(burgerBuilderReducer, composeEnhancers(applyMiddleware(thunk)));
 
 // обернули все приложение в браузер роутер и провайдер
 // стор передается в провайдер с помощью пропса store
