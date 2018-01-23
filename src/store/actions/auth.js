efa19f01431651432da69fa7037d2979f05f73a5
thebat93 <thebat93@sibmail.com>
2018-01-23 12:28:18 +0300

    Getting a Token from the Backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,6 @@
 // экшены, связанные с аутентификацией
 
+import axios from 'axios';
 import * as actionTypes from './actionTypes';
 
 // Action Creators
@@ -25,9 +26,25 @@ export const authFail = (error) => {
 };
 
 // Async Action Creator
+// Не отображается в Redux DevTools
 
 export const auth = (email, password) => {
     return dispatch => {
         dispatch( authStart() );
+        // Данные для отправки
+        const authData = {
+            email, password,
+            returnSecureToken: true
+        };
+        // Отправляем запрос с данными
+        axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCyqOtfIX5Um1BfCT_Y-ZgrTcUNLCxYDtQ', authData)
+            .then(response => {
+                console.log(response);
+                dispatch( authSucess(response.data) ); // запускаем экшен удачной аутентификации
+            })
+            .catch(error => { // В случае ошибки
+                console.log(error);
+                dispatch( authFail(error) ); // запускаем экшен неудачной аутентификации
+            });
     };
 };
