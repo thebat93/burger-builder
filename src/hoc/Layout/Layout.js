682f288a3a06928fc308d96f8150ee48505eb118
thebat93 <ais_93@mail.ru>
2019-01-06 18:28:25 +0700

    Format Layout.js

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -9,42 +9,44 @@ import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 // разметка компонентов
 
 class Layout extends Component {
-    state = {
-        showSideDrawer: false // флаг состояния показа бокового меню
-    }
+  state = {
+    showSideDrawer: false // флаг состояния показа бокового меню
+  };
 
-    closeSideDrawerHandler = () => { // обработчик закрытия бокового меню
-        this.setState({showSideDrawer: false});
-    }
+  closeSideDrawerHandler = () => {
+    // обработчик закрытия бокового меню
+    this.setState({ showSideDrawer: false });
+  };
 
-    sideDrawerToggleHandler = () => { // обработчик переключения сайдбара
-        this.setState((prevState) => {
-            return {showSideDrawer: !prevState.showSideDrawer}
-        });
-    }
-    
-    render() {
-        return (
-            <React.Fragment>
-                <Toolbar
-                    isAuth={this.props.isAuthenticated}
-                    drawerToggleClicked={this.sideDrawerToggleHandler} />
-                <SideDrawer
-                    isAuth={this.props.isAuthenticated}
-                    open={this.state.showSideDrawer} 
-                    closed={this.closeSideDrawerHandler} />
-                <main className={classes.Content}>
-                    { this.props.children }
-                </main>
-            </React.Fragment>
-        );
-    }
+  sideDrawerToggleHandler = () => {
+    // обработчик переключения сайдбара
+    this.setState(prevState => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Toolbar
+          isAuth={this.props.isAuthenticated}
+          drawerToggleClicked={this.sideDrawerToggleHandler}
+        />
+        <SideDrawer
+          isAuth={this.props.isAuthenticated}
+          open={this.state.showSideDrawer}
+          closed={this.closeSideDrawerHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </React.Fragment>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        isAuthenticated: state.auth.token !== null // флаг аутентификации
-    };
+  return {
+    isAuthenticated: state.auth.token !== null // флаг аутентификации
+  };
 };
 
 export default connect(mapStateToProps)(Layout);
